8ba7716eb68dae1fbbeee6ba4c152983e5168e65
Gregory Gerard <wako057@gmail.com>
2024-09-17 11:03:49 +0200

    chore: replace skeleton with new project skeleton

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-import Link from "next/link";
-
-import Header from "@components/header";
-
 export default function Home() {
-  console.log('Executing....  server side')
   return (
     <main>
-      <Header/>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About Us ...</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
