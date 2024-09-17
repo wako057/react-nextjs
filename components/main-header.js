15a25603e39c890b9b0992340d5e4fae6f5c0804
Gregory Gerard <wako057@gmail.com>
2024-09-17 13:38:09 +0200

    feat: styling with css modules

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
     return (
-        <header>
-            <Link href="/">
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
                 <img src={logoImg.src} alt="A plate iwth food on it" />
                 Next Level Food
             </Link>
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                 <li><Link href="/meals">Browse Meals</Link></li>
                 <li><Link href="/community">Foodies Community</Link></li>
