a2fc562cc020e101db3a161b549408d499bcbc7d
Gregory Gerard <wako057@gmail.com>
2024-09-17 14:55:56 +0200

    feat: setup meal grid & items with unknow dimensions

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import classes from './page.module.css';
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
     return (
         <>
-        <h1>Meals Page</h1>
-        <p><Link href="/">Home</Link></p>
-        <p><Link href="/meals/share">Share Page</Link></p>
-        <p><Link href="/meals/youhou">Meals  Dynamic Page</Link></p>
-        <p><Link href="/community">Community Page</Link></p>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meals, created {' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite recipe and cook it yourself. It is easy and fun !
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share your favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
         </>
     );
 }
