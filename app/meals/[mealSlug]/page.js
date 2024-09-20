dc998acaa0687ea0864f6bce7ee2882bf8fcf8ee
Gregory Gerard <wako057@gmail.com>
2024-09-20 10:24:06 +0200

    feat: Meal Details Page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,16 +1,33 @@
-import Link from "next/link";
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+import Image from 'next/image';
 
-export default function MealsDynamic({params}) {
-    console.log(params);
+export default async function MealsDetailsPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
 
     return (
         <>
-            <h1>Meals Dynamic Page</h1>
-
-            <p> Argument Given: <code>{JSON.stringify(params)}</code></p>
-            <p><Link href="/">Home</Link></p>
-            <p><Link href="/meals">Meals Page</Link></p>
-            <p><Link href="/community">Community Page</Link></p>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by a <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }}>
+                </p>
+            </main>
 
         </>
     );
