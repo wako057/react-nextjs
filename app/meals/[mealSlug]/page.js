9490e54eb904199c1a7b871f7d431b18656765d2
Gregory Gerard <wako057@gmail.com>
2024-09-20 15:30:43 +0200

    chore: setup dynaix metadata (SEO need)

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,13 +3,29 @@ import classes from './page.module.css';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({params}) {
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        notFound();
+    }
+    
+    return {
+        title: meal.title,
+        description: meal.summary,
+    }
+};
+
 export default async function MealsDetailsPage({ params }) {
     const meal = getMeal(params.mealSlug);
 
     if (!meal) {
         notFound();
     }
-
+    
+    if (meal &&  meal.instructions) {
+        meal.instructions = meal?.instructions.replace(/\n/g, '<br/>');
+    }
     return (
         <>
             <header className={classes.header}>
